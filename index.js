c23275d6ff61c4c02cba9226a23105349ad1e3a2
avi3175 <avishahriar@.com>
2023-06-10 18:50:46 +0600

    jwt post done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 const port = process.env.PORT || 5000
@@ -78,6 +79,16 @@ async function run() {
 //////////////////////////////////////////////////////////////////////////////////////////
 //****************************************POST*******************************************//
 //////////////////////////////////////////////////////////////////////////////////////////
+
+ app.post('/jwt',(req,res)=>{
+  const user = req.body
+  const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+  res.send({token})
+ })   
+
+
+
+
 
     app.post('/users', async (req, res) => {
       const item = req.body
