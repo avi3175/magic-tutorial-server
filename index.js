ff291f899a8358cc8ce63ebfc01cd9ed9f74b396
avi3175 <avishahriar@.com>
2023-06-12 03:02:31 +0600

    updated stupid verify

--- index.js
+++ index.js
@@ -157,8 +157,8 @@ async function run() {
       res.send(result)
 
     })
-
-    app.get('/users',verifyJWT,verifyAdmin,verifyInstructor, async (req, res) => {
+//PROBLEM//
+    app.get('/users',verifyJWT,verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray()
       res.send(result)
     })
